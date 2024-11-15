9989a01ae78872a27f23a0bf94c3e1170688ec83
njquisel <njquisel@bu.edu>
2024-11-15 16:09:50 -0500

    Added csv files with only column with racist line from deed. Updated csv reader respectively

--- modules/synthetic_data/process_csv.py
+++ modules/synthetic_data/process_csv.py
@@ -2,29 +2,27 @@ import os
 import re
 
 
+
 def retrieve_csv(path):
     #open file
     with open(path, "r", encoding="utf-8") as file:
         csv_content = file.read()
-
-    #create pattern
-    pattern = r",,\"(.*?)\""
-
-    #filter by pattern
-    matches = re.findall(pattern, csv_content)
-
-    #filter duplicates
-    matches = set(matches)
     
-    return matches
+    #get rid of extraneous characters, split by line, remove duplicates
+    csv_content = csv_content.replace("\"","")
+    csv_content = csv_content.split("\n")
+    csv_content = set(csv_content)
+    
+    return csv_content
 
 def retrieve_all_csvs():
     mn_counties = ['washington', 'olmsted', 'dakota']
     res = []
     for county in mn_counties:
-        path = f'./more_racist_deeds/covenants-mn-{county}-county.csv'
+        path = f'modules/synthetic_data/more_racist_deeds/covenants-mn-{county}-county-cleaned.csv'
         res += retrieve_csv(path)
     return res
 
+print(len(retrieve_all_csvs()))
 
     
